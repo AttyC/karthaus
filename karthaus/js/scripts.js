1dc7727bc59b9dce23fe48f1890b968bfe005f46
Antonia Cronin <attycool@hotmail.com>
2015-11-06 12:56:18 +0000

    first commit

---
+++ karthaus/js/scripts.js
@@ -0,0 +1,33 @@
+
+
+$(document).ready(function(){
+
+  /*var homeLinks = [ 
+    { title: "Planning", href: "planning.html", pic1: "img/setts.jpg", pic2: "img/setts_light.jpg"}, 
+    { title: "Architecture", href: "#", pic1: "img/rough.jpg", pic2: "img/rough_light.jpg"}, 
+    { title: "Research", href: "#", pic1: "img/metallic.jpg", pic2: "img/metallic_light.jpg"}, 
+    { title: "Urbanism", href: "#", pic1: "img/fragments.jpg", pic2: "img/fragments_light.jpg"}
+  ];*/
+
+
+  /* display bg image */
+ /*for (var i=0; i<homeLinks.length; i++){
+    console.log(homeLinks[i]);
+    $("#" + i).css({"background": "url(" +  homeLinks[i].pic1 + ") no-repeat", "background-color": "rgb(41, 35, 34)" }); 
+  };
+ 
+$(".bg").mouseenter(function(){
+    $(this).css("background", "url(" + homeLinks[this.id].pic2 + ")");
+    $(this).html("<a target='blank' href=" + homeLinks[this.id].href + "><h3>" + homeLinks[this.id].title + "</h3></a>");
+    
+    }).mouseleave(function() {
+    $(this).css("background", "url(" + homeLinks[this.id].pic1 + ")");
+    $(this).html("");
+    });*/
+
+
+
+
+});
+
+
